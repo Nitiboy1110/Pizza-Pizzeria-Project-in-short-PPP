bbf7d3a1c1745cf87a0c2d834ed59172d0d02009
NITYAM <83535754+Nitiboy1110@users.noreply.github.com>
2021-08-24 20:20:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,24 +3,28 @@
 menu_list_array = ["Veg Margherita Pizza",
                    "Chicken Tandoori Pizza", 
                    "Veg Supereme Pizza",
-                   "Sausage Pizza"];
+                   "Sausage Pizza",
+                   "Double Margherita Pizza"];
 
 function getmenu(){
 var htmldata="<ol class= 'menulist'>"
 menu_list_array.sort();
 for (var i=0;i<menu_list_array.length;i++)
 {
-htmldata=htmldata+ '<li>'+menu_list_array [i] +'</li>'
-   }   htmldata+ "</ol"  
+htmldata=htmldata+ '<li>'+menu_list_array[i] +'</li>'
+   }   htmldata=htmldata+ "</ol>"  
     document.getElementById("display_name").innerHTML = htmldata;
 
 
 }
 
-function add_item(){
+function add_item()
+{
 var htmldata;
 var item=document.getElementById("add_item").value;
+menu_list_array.push(item);
 menu_list_array.sort();
+htmldata="<section class='cards'>"
 for (var i=0;i <menu_list_array.length;i++);
 {
     htmldata=htmldata+'<div class="cards">'
@@ -35,7 +39,5 @@ for (var i=0;i <menu_list_array.length;i++);
     
 function add_top()
 {
-   var item= document.getElementById("add_item").value;
-   menu_list_array.push(item);
-   add_item();
+   document.getElementById("display_addedmenu").innerHTML=htmldata;
 }
